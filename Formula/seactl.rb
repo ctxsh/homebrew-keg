032c0d4f51f8998661aaf9b453540fe8fe7a463d
Rob Lyon <rob@ctxswitch.com>
2024-08-29 18:32:55 -0700

    publish: 0.1.0-pre.10

--- Formula/seactl.rb
+++ Formula/seactl.rb
@@ -1,6 +1,6 @@
 class Seactl < Formula
-  desc "Development environment manager for Kubernetes."
-  homepage ""
+  desc "Manage development environments in kubernetes."
+  homepage "https://github.com/ctxswitch/seaway"
   url "https://github.com/ctxswitch/seaway/archive/refs/tags/v0.1.0-pre.10.tar.gz"
   sha256 "8c0b71e46fcb0fd339cda5ff6a589cfa040473235215e615e0b63101b928da3a"
   license "Apache-2.0"
@@ -8,11 +8,6 @@ class Seactl < Formula
   depends_on "go" => :build
 
   def install
-    # require "net/http"
-    # uri = URI("https://api.github.com/repos/ctxswitch/seaway/releases/latest")
-    # resp = Net::HTTP.get(uri)
-    # resp_json = JSON.parse(resp)
-    # seaway_version = resp_json["tag_name"].sub("v", "")
     ldflags = %W[
       -s -w
       -X ctx.sh/seaway/pkg/build.Version=#{version}
@@ -25,15 +20,6 @@ class Seactl < Formula
 
   test do
     assert_match "seactl version v#{version}", shell_output("#{bin}/seactl --version")
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test seactl`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
     system "false"
   end
 end
