432a1117302a2845c713f6b1325e76c336a9c11c
Rob Lyon <rob@ctxswitch.com>
2024-08-31 11:40:52 -0700

    style fixes

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
+.PHONY: brew-style
+brew-style:
+	@brew style .
+
+.PHONY: brew-style-fix
+brew-style-fix:
+	@brew style --fix .
+
 .PHONY: seactl-bump-to-latest
 seactl-bump-to-latest:
 	@./scripts/bump-to-latest.sh seactl seaway
--- scripts/bump-to-latest.sh
+++ scripts/bump-to-latest.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
-set -x
+set -ex
 USER=ctxswitch
 FORMULA=$1
 REPO=$2
 # RELEASE_URL=$2
 RELEASE_URL="https://api.github.com/repos/${USER}/${REPO}/releases/latest"
 
-if [ -z "$FORMULA" ] || [ -z "$RELEASE_URL" ]; then
+if [[ -z "${FORMULA}" ]] || [[ -z "${RELEASE_URL}" ]]
+then
   echo "Usage: bump-to-latest.sh <formula> <release-url>"
   exit 1
 fi
 
 # Get the latest version from the release URL
-LATEST_VERSION=$(curl -sSL $RELEASE_URL | jq -r '.name')
+RELEASE_DATA=$(curl -sSL "${RELEASE_URL}")
+LATEST_VERSION=$(jq -r '.name' <<<"${RELEASE_DATA}")
 SOURCE_URL="https://github.com/${USER}/${REPO}/archive/refs/tags/${LATEST_VERSION}.tar.gz"
 TMP_FILE=/tmp/${FORMULA}.latest.tar.gz
 # Download the latest version to check the SHA256
-curl -sSL $SOURCE_URL -o ${TMP_FILE}
+curl -sSL "${SOURCE_URL}" -o "${TMP_FILE}"
 # Calculate the SHA256
-SHA256=$(shasum -a 256 $TMP_FILE | awk '{print $1}')
+SHASUM_OUT=$(shasum -a 256 "${TMP_FILE}")
+SHA256=$(awk '{print $1}' <<<"${SHASUM_OUT}")
 
 # Update the formula with the latest version and sha256
 sed -i '' \
   -e "s/\(url \".*\/tags\/\).*.tar.gz\(\".*\)/\1${LATEST_VERSION}\2/" \
   -e "s/\(sha256 \"\).*\(\".*\)/\1${SHA256}\2/" \
-  Formula/${FORMULA}.rb
+  Formula/"${FORMULA}".rb
