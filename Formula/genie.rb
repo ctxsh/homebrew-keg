3f51f867302e4d087640a6592dd7096b3cd00d7d
Rob Lyon <47508711+ctxswitch@users.noreply.github.com>
2024-09-05 23:55:19 +0000

    genie: add 0.1.6 bottle.

--- Formula/genie.rb
+++ Formula/genie.rb
@@ -5,6 +5,13 @@ class Genie < Formula
   sha256 "3e3fd588205aeb1d088f4f315a02b187b177b6347a89238a457783b6ec803d58"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/ctxsh/homebrew-tap/releases/download/genie-0.1.6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "d1d73040d4f5fb4753c0c553cfc65d207ad39b6b62076ff48aef61f5f6572961"
+    sha256 cellar: :any_skip_relocation, ventura:      "ec4659abc38a214b81578ed47a0c17060377db165e5501a5b88e4e6bf46ea091"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d5523d2ce86fae46d26da206593f24aaa4a81c8855ca6076ec77b62cc1ae380f"
+  end
+
   depends_on "go" => :build
 
   def install
